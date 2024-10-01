84d9b8793d0d54fa630c90b039f566409dc3da9f
Casey Cazer <clcazer@gmail.com>
2024-10-01 17:45:42 +0000

    new dashboard with renv

---
+++ SACP_dashboard/ui.R
@@ -0,0 +1,30 @@
+library(shiny)
+library(renv)
+renv::init()
+# Define UI for application that draws a histogram
+fluidPage(
+  
+  # Application title
+  titlePanel("SACP Dashboard"),
+  
+  # Sidebar with a slider input for number of bins 
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput("bins",
+                  "Number of bins:",
+                  min = 1,
+                  max = 50,
+                  value = c(30,50),
+                  width="30%",
+                  dragRange=TRUE),
+    ),
+    
+    position = "right",
+    
+    # Show a plot of the generated distribution
+    mainPanel(
+      plotOutput("distPlot")
+    )
+  )
+)
+
